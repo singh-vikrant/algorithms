ca23fbdf51dce4044b2e49bb3f0d1be70e2c0192
Vikrant Singh <vikrantpbh@gmail.com>
2026-06-04 22:25:16 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] sol = new int[2*n];
+
+        for(int i=0;i<n;i++){
+            sol[i] = sol[i+n] = nums[i];
+        }
+        return sol;
+    }
+}
